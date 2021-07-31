ad1d627f2b224e1c504f38c1f8afee4425bdcbb1
shreenu1995 <65650373+shreenu1995@users.noreply.github.com>
2021-07-31 19:19:52 +0530

    just for test

--- src/main/java/com/devops/phoenix/devopsdemo/DevopsdemoApplication.java
+++ src/main/java/com/devops/phoenix/devopsdemo/DevopsdemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DevopsdemoApplication {
 
-	public static void main(String[] args) {
+	public statics void main(String[] args) {
 		SpringApplication.run(DevopsdemoApplication.class, args);
 	}
 
